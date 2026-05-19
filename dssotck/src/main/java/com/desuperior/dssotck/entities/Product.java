e29eb2f782c8465d73dfb0681004ac7fb4000921
Caiul <caiulpsilva@gmail.com>
2026-05-18 21:20:34 -0300

    Product entity

---
+++ dssotck/src/main/java/com/desuperior/dssotck/entities/Product.java
@@ -0,0 +1,22 @@
+package com.desuperior.dssotck.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    private UUID id;
+    // UUID muito utilizado para poder gerar um codigo aleatorio com ID;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+}
