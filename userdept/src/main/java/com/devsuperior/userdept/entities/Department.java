32218e80d8b152dcb4c228db9686cfbc69df40e5
Caiul <caiulpsilva@gmail.com>
2026-05-20 21:18:14 -0300

    Created SQL

---
+++ userdept/src/main/java/com/devsuperior/userdept/entities/Department.java
@@ -0,0 +1,19 @@
+package com.devsuperior.userdept.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ userdept/src/main/java/com/devsuperior/userdept/entities/User.java
@@ -0,0 +1,26 @@
+package com.devsuperior.userdept.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+
+}
